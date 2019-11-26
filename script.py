460cbff70546738de49a6e3815dc57c8f4902348
rodrigondec <rodrigondec@gmail.com>
2019-11-26 02:05:19 -0300

    script created

---
+++ script.py
@@ -0,0 +1,8 @@
+from data_parser import DataParser
+from horario import Horario
+from materia import Materia
+from professor import Professor
+from turma import Turma
+
+
+dt = DataParser("data/Escola_A.xlsx")
