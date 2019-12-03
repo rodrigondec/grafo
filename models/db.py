2580fd7bed2b5d77bef30228231299aa0a7ac36c
rodrigondec <rodrigondec@gmail.com>
2019-12-02 21:59:53 -0300

    comentários no DB

--- models/db.py
+++ models/db.py
@@ -2,7 +2,18 @@ from utils import classproperty
 
 
 class UniqueCachedModel:
+    """
+    Classe a ser herdada para criar a parsistência dos objetos de sua classe
+    filha em um dicionário (garantindo valor único para cada modelo)
+    """
     def __new__(cls, *args, **kwargs):
+        """
+        Método que cria um novo objeto da classe salvando ele na persistência.
+        Caso um objeto com o identificador já exista, retorna esse objeto.
+        :param args:
+        :param kwargs:
+        :return: objeto achado ou criado
+        """
         if not hasattr(cls, '_instances'):
             cls._instances = {}
 
@@ -16,15 +27,34 @@ class UniqueCachedModel:
 
     @classproperty
     def instances(cls):
+        """
+        Método que retorna o dicionário de persistência da classe.
+        :return:
+        """
         return getattr(cls, '_instances', {})
 
     @classmethod
     def get(cls, key):
+        """
+        Método para pegar um objeto do dicionário de persistência da classe.
+        :param key: identificador do objeto.
+        :return: objeto do identificador OU None (caso objeto não exista)
+        """
         return cls.instances.get(key)
 
 
 class CachedModel:
+    """
+    Classe a ser herdada para criar a parsistência dos objetos de sua classe
+    filha em uma lista (não garantindo valor único para cada modelo)
+    """
     def __new__(cls, *args, **kwargs):
+        """
+        Método que cria um novo objeto da classe salvando ele na persistência.
+        :param args:
+        :param kwargs:
+        :return: objeto criado
+        """
         if not hasattr(cls, '_instances'):
             cls._instances = []
 
@@ -35,8 +65,17 @@ class CachedModel:
 
     @classproperty
     def instances(cls):
+        """
+        Método que retorna a lista de persistência da classe.
+        :return:
+        """
         return getattr(cls, '_instances', [])
 
     @classmethod
     def get(cls, index):
+        """
+        Método para pegar um objeto da lista de persistência da classe.
+        :param index: indice do objeto.
+        :return: objeto do indice
+        """
         return cls.instances[index]
